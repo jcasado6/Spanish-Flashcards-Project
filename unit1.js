5ba98737ab4835e2d168452c77628e861d5770bb
Jose Casado <jcasado6@yahoo.com>
2019-12-04 18:19:13 -0500

    flip

--- unit1.js
+++ unit1.js
@@ -1,3 +1,17 @@
-function flip() {
-    $('.card').toggleClass('flipped');
-}
+console.log('hello')
+
+// let flashcards = document.getElementById('battery')
+
+// flashcards.addEventListener('click', function(){
+//     if (!this.classList.contains('on')){
+//         this.classList.remove('off');
+//         this.classList.add('on');
+//     } else {
+//         this.classList.remove('on');
+//         this.classList.add('off');
+//     }
+// })
+
+$("#battery").click(function(){
+    $(this).toggleClass("flipme");
+  });

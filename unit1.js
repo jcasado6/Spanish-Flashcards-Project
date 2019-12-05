0eb7d222f5f67f2e86165b91998d4c2c9262e86b
Jose Casado <jcasado6@yahoo.com>
2019-12-05 10:12:44 -0500

    flip

--- unit1.js
+++ unit1.js
@@ -1,2 +1,9 @@
 console.log('hello')
 
+let cards = document.querySelector('.flashcards');
+
+function flipCard() {
+  this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));

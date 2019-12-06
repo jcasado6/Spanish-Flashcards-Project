0bfb63d1de78e6defc6d58f95bd92fcdf48b8699
Jose Casado <jcasado6@yahoo.com>
2019-12-06 10:45:51 -0500

    deploying

--- unit1.js
+++ unit1.js
@@ -30,13 +30,16 @@ grabColors.addEventListener('click', function(evt){
     firstSet.style.display = "none"
 })
 
-// let cards = document.querySelector('.flashcards');
+// let cards = document.querySelectorAll('.card');
 
 // function flipCard() {
 //   this.classList.toggle('flip');
 // }
 
 // cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
 
 // var card = document.querySelector('.front');
 
